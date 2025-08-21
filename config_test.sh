e97588b260e5e4b6e6a9d78170c23ec4a4115d6a
Eric Mercer <egm@cs.byu.edu>
2025-08-21 21:13:19 +0000

    Revise config_test.sh to use k-expr for selection

--- config_test.sh
+++ config_test.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 
 if [ $# -ne 1 ]; then
-    echo "usage: sh config_test.sh #"
+    echo "usage: sh config_test.sh <test-name-or-k-expression>"
     exit 1
 fi
 
-printf "[pytest]\npython_files = *$1*" > pytest.ini
+printf '[pytest]\naddopts = -k "%s"\n' "$1" > pytest.ini
 pip install ".[classroom]"
